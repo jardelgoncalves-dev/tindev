622351315ece1acde237545e55ca6c3523023b14
Jardel <jardelgoncalves1996@gmail.com>
2019-08-28 14:49:17 -0300

    add route /dev/:devId/likes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import DevController from './controllers/DevController'
+import LikeController from './controllers/LikeController'
 
 const routes = express.Router()
 
 routes.post('/dev', DevController.store)
+routes.post('/dev/:devId/likes', LikeController.store)
 
 export default routes
