4f2ab8140fa9d242378b74d69bf0653279daccba
Jardel <jardelgoncalves1996@gmail.com>
2019-08-28 15:07:44 -0300

    add route /dev/:devId/dislikes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import DevController from './controllers/DevController'
 import LikeController from './controllers/LikeController'
+import DislikeController from './controllers/DislikeController'
 
 const routes = express.Router()
 
 routes.post('/dev', DevController.store)
 routes.post('/dev/:devId/likes', LikeController.store)
+routes.post('/dev/:devId/dislikes', DislikeController.store)
 
 export default routes
