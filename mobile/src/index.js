de91dba52103cdcac40b3887e1c961a6cd0136c3
Jardel <jardelgoncalves1996@gmail.com>
2019-08-29 20:02:47 -0300

    add pagina e estilização (sem comportamento)

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import Login from './pages/Login'
 
 export default function App () {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!!!</Text>
-    </View>
+    <Login />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#7159c1'
-  },
-  text: {
-    color: '#FFF',
-    fontWeight: 'bold',
-    fontSize: 20
-  }
-})
