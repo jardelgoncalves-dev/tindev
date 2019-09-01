73358bb4174e28d42f367a8d7104fc7f2efe78c0
Jardel <jardelgoncalves1996@gmail.com>
2019-09-01 15:57:53 -0300

    desabilitando warnings

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { YellowBox } from 'react-native'
 import Routes from './routes'
+
+YellowBox.ignoreWarnings([
+  "Unrecognized WebSocket",
+  "Can't perform a React state update"
+])
 
 export default function App () {
   return (
