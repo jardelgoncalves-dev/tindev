7d09efd43ed198678aeb0988d89c1b7705e132fa
Jardel <jardelgoncalves1996@gmail.com>
2019-09-01 02:02:51 -0300

    realizando logica de envio dos dados do usuario para match

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -13,7 +13,16 @@ export default {
     }
 
     if (targetDev.likes.includes(loggedDev._id)) {
-      console.log('deu match')
+      const loggedSocket = req.connectedUsers[user]
+      const targetSocket = req.connectedUsers[devId]
+
+      if (loggedSocket) {
+        req.io.to(loggedSocket).emit('match', targetDev)
+      }
+
+      if (targetSocket) {
+        req.io.to(targetSocket).emit('match', loggedDev)
+      }
     }
 
     if (!loggedDev.likes.includes(targetDev._id)) {
