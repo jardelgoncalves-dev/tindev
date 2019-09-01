e613bc1a4025b9790a6c6d8a6acca907c3702f1d
Jardel <jardelgoncalves1996@gmail.com>
2019-09-01 02:02:11 -0300

    add websocket

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,24 +2,40 @@ import express from 'express'
 import cors from 'cors'
 import ConnMongoose from './config/connection-mongoose'
 import routes from './routes'
+import socket from 'socket.io'
+import http from 'http'
 
 class App {
   constructor () {
     this.express = express()
-
+    this.server = http.Server(this.express)
+    this.io = socket(this.server)
+    this.connectedUsers = {}
     this.config()
+    this.websocket()
     this.middlewares()
     this.routes()
   }
 
   config () {
     ConnMongoose()
-    this.express.set('port', 3333)
+  }
+
+  websocket () {
+    this.io.on('connection', socket => {
+      const { user } = socket.handshake.query
+      this.connectedUsers[user] = socket.id
+    })
   }
 
   middlewares () {
     this.express.use(express.json())
     this.express.use(cors())
+    this.express.use((req, res, next) => {
+      req.io = this.io
+      req.connectedUsers = this.connectedUsers
+      return next()
+    })
   }
 
   routes () {
@@ -27,4 +43,4 @@ class App {
   }
 }
 
-export default new App().express
+export default new App().server
